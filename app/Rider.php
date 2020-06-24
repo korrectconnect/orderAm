24e167b7eb21a403e603600220633ad6c96a34a9
olamidehimself <egundeyiolamide@gmail.com>
2020-06-24 15:00:40 +0100

    remove fillables from Riders model

--- app/Rider.php
+++ app/Rider.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rider extends Model
 {
-    protected $fillable = ['first_name', 'last_name', 'profile_picture', 'address', 'phone_number', 'email', 'age'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+//
 }
